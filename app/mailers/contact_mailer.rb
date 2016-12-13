9729f9e47e9df511caea81510cb4a4b21b0e0631
Wesley McLachlan <saikohcoder@gmail.com>
2016-12-13 07:41:18 +0000

    Added contact mailer to view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default to: 'saikohcoder@gmail.com'
   
-  def contract_email(name, email, body)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body

9e357cff3fbb66365ba3fa57abc6bbe5b8bb7fda
Wesley McLachlan <saikohcoder@gmail.com>
2016-12-21 10:35:34 +0000

    Updated CSS of website to be more appealing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resource :profile
   end
-  get 'about', to: 'pages#about'
+  # get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
